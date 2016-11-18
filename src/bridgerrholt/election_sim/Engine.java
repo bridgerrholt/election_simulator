e521788e1a5b6efc4bbd178781e862a8e2fbc4e8
bridgerholt <bridgerrholt@gmail.com>
2016-11-18 13:25:10 -0700

    Improved database setup method

--- src/bridgerrholt/election_sim/Engine.java
+++ src/bridgerrholt/election_sim/Engine.java
@@ -27,26 +27,12 @@ public class Engine {
 			this.candidates = new ArrayList<>();
 		else
 			this.candidates = candidates;
-
-
 	}
 
 	public void setup(String databasePath) throws Exception {
 		connection = Database.createConnection(databasePath);
 
-		List<String> lines = Files.readAllLines(
-			Paths.get("database_setup.txt"), Charset.defaultCharset()
-		);
-
-		for (String i : lines) {
-			Statement statement = connection.createStatement();
-			while (statement.execute(i)) {
-
-			}
-		}
-
-
-
+		SetupReader setupReader = new SetupReader(connection, "database_setup.txt");
 	}
 
 
@@ -56,3 +42,43 @@ public class Engine {
 }
 
 
+
+class SetupReader {
+	SetupReader(Connection connection, String setupPath) throws Exception {
+		List<String> lines = Files.readAllLines(
+			Paths.get(setupPath), Charset.defaultCharset()
+		);
+
+		State state = State.NONE;
+
+		for (String line : lines) {
+			line = line.trim();
+
+			if (line.equals("tables:"))
+				state = State.TABLES;
+
+			else {
+				Statement statement = connection.createStatement();
+				switch (state) {
+					case NONE:
+						while (statement.execute(line)) { }
+						break;
+
+					case TABLES:
+						String queryText = "CREATE TABLE IF NOT EXISTS ".concat(line);
+						while (statement.execute(queryText)) { }
+						break;
+
+					default:
+						throw new Exception("Invalid state within SetupReader");
+				}
+			}
+		}
+	}
+
+
+	private enum State {
+		NONE,
+		TABLES
+	}
+}

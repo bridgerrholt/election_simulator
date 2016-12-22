6d5f72a7927acc895296ce654735f206205d7a2e
bridgerrholt <bridgerrholt@gmail.com>
2016-12-21 21:21:17 -0700

    Generator working

--- src/bridgerrholt/election_sim/execution/SimulationGenerator.java
+++ src/bridgerrholt/election_sim/execution/SimulationGenerator.java
@@ -29,6 +29,20 @@ public class SimulationGenerator implements Execution {
 			"SELECT * FROM regional_opinion_overview"
 		);
 
+		PreparedStatement regionalOpinionInsert =
+			simulationConnection.prepareStatement(
+				"INSERT INTO regional_opinions " +
+				"(topic_id, region_id, option_list_id) VALUES " +
+				"(?, ?, ?);"
+			);
+
+		PreparedStatement opinionListInsert =
+			simulationConnection.prepareStatement(
+				"INSERT INTO opinion_lists " +
+				"(list_id, scale_index, person_count) VALUES " +
+				"(?, ?, ?)"
+			);
+
 		ResultSet resultSet = overview.executeQuery();
 
 
@@ -42,11 +56,26 @@ public class SimulationGenerator implements Execution {
 				resultSet.getInt("favor_intensity")
 			);
 
+			int nextListId = getNextOpinionListId();
+
+			regionalOpinionInsert.setInt(1, topicId);
+			regionalOpinionInsert.setInt(2, regionId);
+			regionalOpinionInsert.setInt(3, nextListId);
+			regionalOpinionInsert.execute();
+
+
 			for (Integer i : peopleCounts) {
 				System.out.print(i + " ");
 			}
 
 			System.out.println();
+
+			for (int i = 0; i < peopleCounts.size(); ++i) {
+				opinionListInsert.setInt(1, nextListId);
+				opinionListInsert.setInt(2, i);
+				opinionListInsert.setInt(3, peopleCounts.get(i));
+				opinionListInsert.execute();
+			}
 
 			/*PreparedStatement regionalOpinionInsert = simulationConnection.prepareStatement(
 				"INSERT INTO regional_opinions (list_id, scale_index, person_count) " +
@@ -132,6 +161,23 @@ public class SimulationGenerator implements Execution {
 
 
 		return peopleCounts;
+	}
+
+	private int getNextOpinionListId() throws java.sql.SQLException {
+		PreparedStatement statement =
+			simulationConnection.prepareStatement(
+				"SELECT MAX(list_id) FROM opinion_lists"
+			);
+
+		ResultSet result = statement.executeQuery();
+
+		if (!result.next()) {
+			return 1;
+		}
+		else {
+			int max = result.getInt(1);
+			return max + 1;
+		}
 	}
 
 	private Connection     simulationConnection;
